57d8674701b285e2f2a3509fe97c01ebdf5789cb
Roger <rogermphile1991@gmail.com>
2016-02-29 13:47:25 +0200

    I have added a BeaconManagerListener to detect a new beacon, update a previous scanned beacon and to know if a Beacon has been lost.

--- app/src/main/java/za/co/com/state/empire/clocking/MainActivity.java
+++ app/src/main/java/za/co/com/state/empire/clocking/MainActivity.java
@@ -1,21 +1,41 @@
 package za.co.com.state.empire.clocking;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.sensoro.beacon.kit.Beacon;
 import com.sensoro.beacon.kit.BeaconManagerListener;
 import com.sensoro.cloud.SensoroManager;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_ENABLE_BT = 100;
+    public static final String YunziSN = "0117C5393A7A";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    String mBeaconUDID = "";
+
+    public static final int STATE_INIT = 0;
+    public static final int STATE_ENTER_DOOR = 1;
+    public static final int STATE_IN_ROOM = 2;
+    public static final int STATE_OUT_ROOM_ENTER_DOOR = 3;
+    public Date mInTime;
+    public Date mOutTime;
+    public int mState = STATE_INIT;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeSDK();
     }
 
     public void initializeSDK() {
@@ -39,8 +59,26 @@ public class MainActivity extends AppCompatActivity {
 
             sensoroManager.addBroadcastKey("0117C5393A7A");
             BeaconManagerListener beaconManagerListener = new BeaconManagerListener() {
+
                 @Override
                 public void onNewBeacon(Beacon beacon) {
+                    // New Senso found
+                    if (beacon.getSerialNumber().equals(YunziSN)){
+                        System.out.println("New Beacon");
+                    }
+
+                    Log.e(TAG, beacon.getProximityUUID());
+
+                    String beaconUUID = beacon.getProximityUUID();
+                    if (beaconUUID.equals(mBeaconUDID)){
+                        if (mState == STATE_INIT){
+                            mState = STATE_ENTER_DOOR;
+                        }else if (mState == STATE_IN_ROOM){
+                            mState = STATE_OUT_ROOM_ENTER_DOOR;
+                            mOutTime = new Date();
+                            Log.e(TAG, "out room");
+                        }
+                    }
 
                 }
 
@@ -54,6 +92,10 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             };
+            sensoroManager.setBeaconManagerListener(beaconManagerListener);
+        }else {
+            Intent blue = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(blue, REQUEST_ENABLE_BT);
         }
     }
 }
