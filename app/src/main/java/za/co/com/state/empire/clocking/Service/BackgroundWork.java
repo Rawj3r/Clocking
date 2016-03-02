11192be88f6c431eb2504f452a014eb810aa175e
Roger <rogermphile1991@gmail.com>
2016-03-02 10:46:39 +0200

    New TransActivity class created

--- app/src/main/java/za/co/com/state/empire/clocking/Service/BackgroundWork.java
+++ app/src/main/java/za/co/com/state/empire/clocking/Service/BackgroundWork.java
@@ -2,6 +2,7 @@ package za.co.com.state.empire.clocking.Service;
 
 import android.app.Application;
 import android.content.Intent;
+import android.util.Log;
 
 import com.sensoro.beacon.kit.Beacon;
 import com.sensoro.beacon.kit.BeaconManagerListener;
@@ -20,6 +21,9 @@ public class BackgroundWork extends Application implements BeaconManagerListener
     String mBeaconUDID = "";
     public Date mInTime;
     public Date mOutTime;
+    public static final int STATE_ENTER_DOOR = 1;
+    public int mState = STATE_INIT;
+    public static final int STATE_INIT = 0;
 
     @Override
     public void onCreate() {
@@ -32,15 +36,77 @@ public class BackgroundWork extends Application implements BeaconManagerListener
 
         Intent intent = new Intent();
         intent.setClass(this, MyService.class);
+        startService(intent);
     }
 
+    /**
+     * Initial Sensoro SDK
+     */
     private void initSensorSDK() {
+        sensoroManager = SensoroManager.getInstance(getApplicationContext());
+        sensoroManager.setCloudServiceEnable(true);
+        sensoroManager.setBeaconManagerListener(this);
+    }
 
+
+    /**
+     * Start Sensoro SDK
+     * @param
+     */
+
+    public void startSensorSDK(){
+        mBeaconUDID = "";
+        mState = STATE_INIT;
+        Intent intent = new Intent("changeState");
+        sendBroadcast(intent);
+        try {
+            sensoroManager.startService();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void stopSensoroSDK(){
+        mBeaconUDID = "";
+        mState = STATE_INIT;
+        Intent intent = new Intent("changeState");
+        sendBroadcast(intent);
+        try {
+            sensoroManager.stopService();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether bluetooth enabled.
+     * @return
+     */
+
+    public boolean isBlueteethEnabled(){
+        return sensoroManager.isBluetoothEnabled();
     }
 
     @Override
     public void onNewBeacon(Beacon beacon) {
 
+        Log.e(TAG, beacon.getProximityUUID());
+        String beaconUUID = beacon.getProximityUUID();
+
+        if (beaconUUID.equals(mBeaconUDID)){
+            if (mState == STATE_INIT){
+                mState = STATE_ENTER_DOOR;
+            }else {
+                mState = STATE_INIT;
+            }
+        }
+
+
+        mBeaconUDID = beaconUUID;
+        Intent intent = new Intent("changeState");
+        sendBroadcast(intent);
+
+        Intent intent1 = new Intent(this, TransActivity.class);
     }
 
     @Override
