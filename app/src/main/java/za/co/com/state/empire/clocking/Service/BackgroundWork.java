2d740caec83f0cb0b04a181d125d54ea85f4f6cb
Roger <rogermphile1991@gmail.com>
2016-03-02 10:23:05 +0200

    new Service class created

--- app/src/main/java/za/co/com/state/empire/clocking/Service/BackgroundWork.java
+++ app/src/main/java/za/co/com/state/empire/clocking/Service/BackgroundWork.java
@@ -1,11 +1,14 @@
 package za.co.com.state.empire.clocking.Service;
 
 import android.app.Application;
+import android.content.Intent;
 
 import com.sensoro.beacon.kit.Beacon;
 import com.sensoro.beacon.kit.BeaconManagerListener;
+import com.sensoro.cloud.SensoroManager;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by empirestate on 3/2/16.
@@ -13,6 +16,28 @@ import java.util.ArrayList;
 public class BackgroundWork extends Application implements BeaconManagerListener{
 
     private static final String TAG = BackgroundWork.class.getSimpleName();
+    private SensoroManager sensoroManager;
+    String mBeaconUDID = "";
+    public Date mInTime;
+    public Date mOutTime;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initSensorSDK();
+
+        /**
+         * Start SDK in Serevice
+         */
+
+        Intent intent = new Intent();
+        intent.setClass(this, MyService.class);
+    }
+
+    private void initSensorSDK() {
+
+    }
+
     @Override
     public void onNewBeacon(Beacon beacon) {
 
